b0012d61629524317ed81990397d428c7884aa86
peter-tank <30540412+peter-tank@users.noreply.github.com>
2023-10-28 15:00:00 +0000

    set default action for vivo PD2054 * try vivo common kernel hard patches
    Signed-off-by: peter-tank <30540412+peter-tank@users.noreply.github.com>

--- magisk_patch.sh
+++ magisk_patch.sh
@@ -22,24 +22,26 @@ adb wait-for-device
 echo device found
 
 adb push magisk/lib/x86_64/libmagiskboot.so /data/local/tmp/magiskboot
-adb push magisk/lib/$CPU_ABI/libmagiskinit.so /data/local/tmp/magiskinit
 
-if [ "$CPU_ABI" = "arm64-v8a" ] || [ "$CPU_ABI" = "armeabi-v7a" ];then
-    if [ "$CPU_ABI" = "arm64-v8a" ]; then
-        adb push magisk/lib/arm64-v8a/libmagisk64.so /data/local/tmp/magisk64
-    fi
-    adb push magisk/lib/armeabi-v7a/libmagisk32.so /data/local/tmp/magisk32
-elif [ "$CPU_ABI" = "x86_64" ] || [ "$CPU_ABI" = "x86" ];then
-    if [ "$CPU_ABI" = "x86_64" ]; then
-        adb push magisk/lib/x86_64/libmagisk64.so /data/local/tmp/magisk64
-    fi
-    adb push magisk/lib/x86/libmagisk32.so /data/local/tmp/magisk32
-else
-    echo "cpu_abi:"$CPU_ABI" may not be supported"
-fi
+_bits=32
+if grep -q "64" <<< "$CPU_ABI"; then _bits=64; fi
+for _fn in "magiskinit" "magisk${_bits}"; do
+    adb push "magisk/lib/$CPU_ABI/lib${_fn}.so" "/data/local/tmp/${_fn}"
+    adb shell chmod 0755 "/data/local/tmp/${_fn}"
+done
 
+echo "Try: leave kernel patch."
+sed -ie 's/0092CFC2C9CEC0DB00$/0092CFC2C9CEC0DB00 \&\& PATCHEDKERNEL=true/g' magisk/assets/boot_patch.sh
+grep -HC3 0092CFC2C9CEC0DB00 magisk/assets/boot_patch.sh
 adb push magisk/assets/boot_patch.sh /data/local/tmp/
 adb shell chmod 755 /data/local/tmp/boot_patch.sh
+
+adb push magisk/assets/stub.apk /data/local/tmp/
+#adb push stub.apk /data/local/tmp/
+
+echo "Fix: magisk/assets/util_functions.sh"
+sed -ie 's/]$/]; then/g' magisk/assets/util_functions.sh
+head -n 335 magisk/assets/util_functions.sh | tail
 adb push magisk/assets/util_functions.sh /data/local/tmp/
 
 for bootimage in `find imgs -name boot.img -o -name init_boot.img`;do
